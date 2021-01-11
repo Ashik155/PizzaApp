81d0b8f38ea42fe2147cabe058f3322891a338f0
ashikpatel-doof <ashikpatel.doof@gmail.com>
2021-01-11 14:41:18 +0530

    LOGIN REGISTER

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const connectDb = require("./app/config/db")
 const session = require("express-session")
 const flash = require('express-flash')
 const MongoStore = require('connect-mongo')(session)
+const passport = require('passport')
+const passportInit = require("./app/config/pass")
 
 
 
@@ -21,16 +23,22 @@ connection.once('open', () => {
 });
 
 
+
+
 //Defining Port Number ...
 const PORT = process.env.PORT || 2000
 
 //registering APP
 const app = express()
+//passport setup.....
+
 
 //serving Static Views and content ...
 app.use(express.static("public"))
 //jsonsetting 
 app.use(express.json())
+//accpet data from url 
+app.use(express.urlencoded({extended:false}))
 
 
 // Session store
@@ -47,6 +55,10 @@ app.use(session({
 }))
 app.use(flash())
 
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 //registering templating Enginee...and setting up views ,...
 app.use(expressLayouts)
@@ -57,6 +69,7 @@ app.set('view engine', 'ejs' )
 app.use((req,res,next)=>{
     if(req.session){
         res.locals.session = req.session
+        res.locals.user = req.user
     console.log("req.session is ", req.session)
     next()
 
